071aad6dd7f3d011fa79fbced9b5743a63ecd6b1
Mohit Arora <mohitarora7272@gmail.com>
2024-10-18 22:30:58 +0530

    feature new commit with changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8d2b9c0d28a1ce81c9557f81614957b2
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "ed10e420e6d79b8cdbd5c99a475997f1",
+"flutter_bootstrap.js": "f5fec204a408b7999bdfc5d3db595cce",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
